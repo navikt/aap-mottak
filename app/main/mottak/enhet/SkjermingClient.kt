bf00702f54d0f2194b66734a93c3ab671ab02ac3
Richard Borge <richard.borge@nav.no>
2024-03-26 13:55:32 +0100

    Setter på auth for skjerming

--- app/main/mottak/enhet/SkjermingClient.kt
+++ app/main/mottak/enhet/SkjermingClient.kt
@@ -7,23 +7,27 @@ import mottak.Config
 import mottak.Ident
 import mottak.http.HttpClientFactory
 import mottak.http.tryInto
+import no.nav.aap.ktor.client.auth.azure.AzureAdTokenProvider
 import java.net.URI
 
 interface Skjerming {
     fun isSkjermet(personident: Ident.Personident): Boolean
 }
 
-class SkjermingClient(config: Config) : Skjerming {
+class SkjermingClient(private val config: Config) : Skjerming {
     private val httpClient = HttpClientFactory.default()
     private val host: URI = config.skjerming.host
+    private val tokenProvider = AzureAdTokenProvider(config.azure, httpClient)
 
     override fun isSkjermet(personident: Ident.Personident): Boolean {
         val body = SkjermingReq(personident.id)
 
         return runBlocking {
+            val token = tokenProvider.getClientCredentialToken(config.skjerming.scope)
             val response = httpClient.post("$host/skjermet") {
                 contentType(ContentType.Application.Json)
                 accept(ContentType.Application.Json)
+                bearerAuth(token)
                 setBody(body)
             }
 
